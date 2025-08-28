457c2b2fee82e5fa5686c07f52b018604dae8e9a
Johan Jönsson <jonsson.johan@outlook.com>
2025-08-28 09:29:21 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  }
 })
